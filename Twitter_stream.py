118414cd6c79565de17d93b3b8cdc8f9e691644d
fiercealfalfa <allewellyn@gmail.com>
2015-04-21 15:50:43 -0500

    Core Streaming and OAuth Complete
    Also added keys.py and keys.pyc to gitignore

--- Twitter_stream.py
+++ Twitter_stream.py
@@ -1,18 +1,33 @@
 #Streaming Data from Twitter
 import tweepy
-
-#Read API and Consumer Keys
-
-keys_file = open('keys.txt','r')
-token_list = keys_file.readlines()
+from tweepy.streaming import StreamListener
+from keys import twitter_keys
 
 #API and Consumer Keys
-access_token = token_list[0]
-access_token_secret = token_list[1]
-consumer_key = token_list[2]
-consumer_secret = token_list[3]
+access_token = twitter_keys['a_token']
+access_token_secret = twitter_keys['a_token_s']
+consumer_key = twitter_keys['c_key']
+consumer_secret = twitter_keys['c_secret']
 
+#prints tweets to console
+class StdOutListener(StreamListener):
+	def on_data(self, data):
+		print data
+		return True
+	def on_error(self, status):
+		print status
+		return True
 
+#Program Starts here
+if __name__ == '__main__':
+	#Authentication
+	needs_auth = StdOutListener()
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
+	stream = tweepy.Stream(auth, needs_auth)
+		
+	#Here is the filter
+	stream.filter(track = ["the"])
 
 
 
